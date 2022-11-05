21225757cb7e1b348d4b1bb47cde9d142589592a
Hoangxt <dinhhuyhoangxt@gmail.com>
2022-11-05 23:45:13 +0700

    Social Media User Story Section UI Design

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,33 +1,14 @@
 import React from "react";
+import Posts from "../../components/posts/Posts";
+import Stories from "../../components/stories/Stories";
 
 import "./home.scss";
 
 const Home = () => {
   return (
     <div className="home">
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
-      <h1>Home</h1>Home
+      <Stories />
+      <Posts />
     </div>
   );
 };
