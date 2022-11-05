645fde853ac71dfbbae3f9f62dd174610bd9236b
Hoangxt <dinhhuyhoangxt@gmail.com>
2022-11-05 19:28:28 +0700

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+      <h1>Home</h1>Home
+    </div>
+  );
+};
+
+export default Home;
